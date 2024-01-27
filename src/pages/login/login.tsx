080482bdd11e13cd9229f2516adf0e08079a238e
TahaTaqa <122167277+TahaTaqa@users.noreply.github.com>
2024-01-27 10:27:30 +0300

    Update login.tsx

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -93,7 +93,7 @@ export const Login = () => {
         onSubmit={(e: React.SyntheticEvent) => handleSubmit(e, values)}
         className="w-1/2 lg:w-1/3 flex flex-col gap-5 justify-center items-start"
       >
-        <h1 className="text-2xl text-blue-500 ml-20 ">تسجيل دخول</h1>
+        <h1 className="text-2xl text-blue-500 ml-20 ">تسجيل الدخول</h1>
 
         <TextField
           id="email"
